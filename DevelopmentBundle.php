bc7bf1ee12c6f3c355888668de26bcacdef05825
Simon Asika <asika32764@gmail.com>
2016-04-01 13:43:14 +0800

    First Commit

---
+++ DevelopmentBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Part of joomlarad project.
+ *
+ * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace DevelopmentBundle;
+
+include_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * The DevelopmentBundle class.
+ *
+ * @since  {DEPLOY_VERSION}
+ */
+class DevelopmentBundle extends \Windwalker\Bundle\AbstractBundle
+{
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Windwalker Joomla RAD Development Bundle
+
+## Installation via Composer
+
+``` bash
+cd /to/your/joomla
+
+composer create-project windwalker/rad-development-bundle libraries/windwalker-bundles/DevelopmentBundle 1.*
+```
+
+
